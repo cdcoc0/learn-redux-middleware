faf33661a06dad16d8e8aef5f3b2410b92029c8e
velykitty <jiyunee13@gmail.com>
2021-05-13 12:39:18 +0900

    Create middleware -미들웨어의 기본 구조

---
+++ src/lib/loggerMiddleware.js
@@ -0,0 +1,15 @@
+const loggerMiddleware = store => next => action => {
+    //미들웨어의 기본 구조
+}
+
+export default loggerMiddleware;
+
+
+
+// const loggerMiddleware - function loggerMiddleware(Store) {
+//     return function(next) {
+//         return function(action) {
+//             미들웨어 기본 구조!
+//         }
+//     }
+// }
