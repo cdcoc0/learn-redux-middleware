c3062c8a381cc3b22d3624ede082e59232ff9c2a
velykitty <jiyunee13@gmail.com>
2021-05-14 18:27:10 +0900

    Refactoring -thunk함수 분리

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import {handleActions} from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 //액션 타입 선언
 //한 요청당 세 개 생성
@@ -8,46 +9,48 @@ const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
 const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
-const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCCESS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 //thunk 함수 생성
 //thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치
-export const getPost = id => async dispatch => {
-    dispatch({type: GET_POST}); //요청 시작을 알림
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        });
-    } catch(e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-        });
-        throw e; //나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
-    }
-};
+// export const getPost = id => async dispatch => {
+//     dispatch({type: GET_POST}); //요청 시작을 알림
+//     try {
+//         const response = await api.getPost(id);
+//         dispatch({
+//             type: GET_POST_SUCCESS,
+//             payload: response.data
+//         });
+//     } catch(e) {
+//         dispatch({
+//             type: GET_POST_FAILURE,
+//             payload: e,
+//             error: true
+//         });
+//         throw e; //나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
+//     }
+// };
 
-export const getUsers = id => async dispatch => {
-    dispatch({type: GET_USERS});
-    try {
-        const response = await api.getUsers(id);
-        dispatch({
-            type: GET_USERS_SUCCESS,
-            payload: response.data
-        });
-    } catch(e) {
-        dispatch({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-        });
-        throw e;
-    }
-};
+// export const getUsers = id => async dispatch => {
+//     dispatch({type: GET_USERS});
+//     try {
+//         const response = await api.getUsers(id);
+//         dispatch({
+//             type: GET_USERS_SUCCESS,
+//             payload: response.data
+//         });
+//     } catch(e) {
+//         dispatch({
+//             type: GET_USERS_FAILURE,
+//             payload: e,
+//             error: true
+//         });
+//         throw e;
+//     }
+// };
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 //초기 상태 선언
 //요청의 로딩 중 상태는 loading이라는 객체에서 관리
