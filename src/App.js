b886664b81b60c0e0bcedde69c554c3aa1a0592c
velykitty <jiyunee13@gmail.com>
2021-05-14 12:19:36 +0900

    Redux thunk 사용해 웹 요청 비동기 작업 처리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
+import SampleContainer from './containers/SampleContainer';
 
 function App() {
   return (
     <div>
       <CounterContainer />
+      <SampleContainer />
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,7 @@ const Counter = ({onIncrease, onDecrease, number}) => {
             <h1>{number}</h1>
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
+            <hr />
         </div>
     );
 };
