bf92cee75e7fbcd887fe1c7229d87bac80d10284
velykitty <jiyunee13@gmail.com>
2021-05-15 22:00:35 +0900

    Redux-saga -sample 컴포넌트에 적용

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,6 +1,8 @@
-import {handleActions} from 'redux-actions';
+import {createAction, handleActions} from 'redux-actions';
+import {call, put, takeLatest} from 'redux-saga/effects';
 import * as api from '../lib/api';
 import createRequestThunk from '../lib/createRequestThunk';
+import {startLoading, finishLoading} from './loading';
 
 //액션 타입 선언
 //한 요청당 세 개 생성
@@ -49,8 +51,58 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 //         throw e;
 //     }
 // };
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+
+// export const getPost = createRequestThunk(GET_POST, api.getPost);
+// export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+
+export const getPost = createAction(GET_POST, id => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+    yield put(startLoading(GET_POST)); //로딩 시작
+    
+    //파라미터로 actionㅇ르 받아 오면 액션의 정보를 조회할 수 있음
+    try {
+        //call을 사용하면 Promise를 반환하는 함수를 호출하고, 기다릴 수 있음
+        //첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수
+        const post = yield call(api.getPost, action.payload); //api.getPost(action.payload)를 의미
+        yield put({
+            type: GET_POST_SUCCESS,
+            payload: post.data
+        });
+    } catch(e) {
+        //try/catch문을 사용해 에러도 잡을 수 있음
+        yield put({
+            type: GET_POST_FAILURE,
+            payload: e,
+            error: true
+        });
+    }
+    yield put(finishLoading(GET_POST)); //로딩 완료
+}
+
+function* getUsersSaga() {
+    yield put(startLoading(GET_USERS));
+    try {
+        const users = yield call(api.getUsers);
+        yield put({
+            type: GET_USERS_SUCCESS,
+            payload: users.data
+        });
+    } catch(e) {
+        yield put({
+            type: GET_USERS_FAILURE,
+            payload: e,
+            error: true
+        });
+    }
+    yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+    yield takeLatest(GET_POST, getPostSaga);
+    yield takeLatest(GET_USERS, getUsersSaga);
+};
 
 //초기 상태 선언
 //요청의 로딩 중 상태는 loading이라는 객체에서 관리
